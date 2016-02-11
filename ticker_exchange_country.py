7add4f610814b91d6121cee2feacd09a7a107dec
LEEMEI HSIUAN <williamchen180@gmail.com>
2016-02-11 10:18:02 +0800

    commit

--- ticker_exchange_country.py
+++ ticker_exchange_country.py
@@ -8,19 +8,36 @@ book = open_workbook('Yahoo Ticker Symbols - Jan 2016.xlsx')
 
 sheet = book.sheet_by_name('Stock')
 
-idx = 4 
 
 T = {}
 
-for name in sheet.col(0)[4:]:
-	short = sheet.col(1)[idx].value
-	e = sheet.col(2)[idx].value
-	c = sheet.col(3)[idx].value
-	t = name.value
+t_array = []
+s_array = []
+e_array = []
+c_array = []
 
-	idx += 1
-	print t, c, short 
 
+for t in sheet.col(0)[4:]:
+	t_array.append(t.value)
+for t in sheet.col(1)[4:]:
+	s_array.append(t.value)
+for t in sheet.col(2)[4:]:
+	e_array.append(t.value)
+for t in sheet.col(3)[4:]:
+	c_array.append(t.value)
+
+idx = 0
+
+print len(t_array)
+print len(s_array)
+print len(e_array)
+print len(c_array)
+
+for t in t_array:
+	s = s_array[idx]
+	e = e_array[idx]
+	c = c_array[idx]
+	
 	if T.has_key( c ) == False:
 		T[c] = {} 
 	T[c][t] = {}
@@ -28,31 +45,7 @@ for name in sheet.col(0)[4:]:
 	T[c][t]['INDEX'] = idx
 	T[c][t]['COUNTRY'] = c
 	T[c][t]['SYMBOL'] = t
-	T[c][t]['SHORT'] = short
-
-
-if False:
-	idx = 0
-	with open('ticker.txt','r') as ticket:
-		with open('exchange.txt','r') as exchange:
-			with open('country.txt','r') as country:
-
-				for t in ticket.readlines():
-					e = exchange.readline()
-					c = country.readline()
-
-					t = t.split('\n')[0]
-					e = e.split('\n')[0]
-					c = c.split('\n')[0]
-					
-					if T.has_key( c ) == False:
-						T[c] = {} 
-					T[c][t] = {}
-					T[c][t]['EXCHANGE'] = e
-					T[c][t]['INDEX'] = idx
-					T[c][t]['COUNTRY'] = c
-					T[c][t]['SYMBOL'] = t
-					idx += 1
+	T[c][t]['SHORT'] = s
 
 with open('pickle/ticker.cpickle2', 'wb') as f:
 	cPickle.dump( T, f, protocol=2)
