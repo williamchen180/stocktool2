528ab85417afb6da90542ca411d7ab017a930987
LEEMEI HSIUAN <williamchen180@gmail.com>
2016-02-26 00:32:01 +0800

    Add 1% range in maxium y so that there will be some space up above.

--- plot.py
+++ plot.py
@@ -146,8 +146,10 @@ class	plot:
 		p('set size 1, 0.4')
 		p('set origin 0, 0.6')
 		if os.path.isfile( ROI_file ) is False:
+			print 'here'
 			p('plot RRI9(x) title "9%",RRI11(x) title "11%", RRI14(x) title "14%", "' + price_file + '" using 1:7 notitle with lines')
 		else:
+			print 'there'
 			cmd = 'plot "%s" using 1:3 title "9%%" with lines, "%s" using 1:4 title "11%%" with lines, "%s" using 1:5 title "14%%" with lines, "%s" using 1:5 notitle with lines' % (ROI_file, ROI_file, ROI_file, price_file )
 
 			p(cmd)
@@ -174,6 +176,9 @@ class	plot:
 		p('set bmargin')
 		p('set xrange [ "%s":"%s" ]' % (left_x, right_x))
 		p('plot "' + dividend_file + '" using 1:2 title "dividend" with linespoints')
+		p('clear')
+		p('set yr [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX *1.01]')
+		p('replot')
 		p('unset multiplot')
 
 		#time.sleep(1)
